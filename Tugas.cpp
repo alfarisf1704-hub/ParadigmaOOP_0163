24f568a1589557e39cd481c34cdd1fd30034acca
alfarisf1704-hub <alfarisf1704@gmail.com>
2026-04-20 14:46:38 +0700

    membuat class barang

--- Tugas.cpp
+++ Tugas.cpp
@@ -1,27 +1,6 @@
 #include <iostream>
 using namespace std;
 
-class Mahasiswa
+class Barang
 {
-public:
-    int nim;
-    string nama;
-    float nilai;
-
-    void printData()
-    {
-        cout << "NIM : " << nim << endl;
-        cout << "Nama : " << nama << endl;
-        cout << "Nilai : " << nilai << endl;
-    }
-}; // batas class
-
-int main()
-{
-    Mahasiswa mhs;
-    mhs.nim = 2022;
-    mhs.nama = "Fauzan";
-    mhs.nilai = 7.7;
-
-    mhs.printData();
 }
