45576755ca11e4f96b3edbef8897b95621d13506
alfarisf1704-hub <alfarisf1704@gmail.com>
2026-04-20 14:40:24 +0700

    menambahkan class

---
+++ Tugas.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+class Mahasiswa
+{
+public:
+    int nim;
+    string nama;
+    float nilai;
+
+    void printData()
+    {
+        cout << "NIM : " << nim << endl;
+        cout << "Nama : " << nama << endl;
+        cout << "Nilai : " << nilai << endl;
+    }
+}; // batas class
+
+int main()
+{
+    Mahasiswa mhs;
+    mhs.nim = 2022;
+    mhs.nama = "Fauzan";
+    mhs.nilai = 7.7;
+
+    mhs.printData();
+}
